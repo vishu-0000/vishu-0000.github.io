8f093d263bee597000bd897c125a629f8907df49
vidushi-agarwal <vidushiagarwal1997@gmail.com>
2018-04-10 18:08:25 +0530

    .

--- js/custom.js
+++ js/custom.js
@@ -162,6 +162,25 @@ particlesJS("particles-js", {
       e.preventDefault();
       jQuery('.probootstrap-header-top').toggleClass('active');
     });
+    // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 57
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
   };
 
 
